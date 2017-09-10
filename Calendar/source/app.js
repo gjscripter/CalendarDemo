bb89c795809669523fde5570b0ecf0e4f0474b12
gjscripter <30704493+gjscripter@users.noreply.github.com>
2017-09-10 18:48:38 +0100

    Add files via upload

---
+++ Calendar/source/app.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Calendar = require('./components/Calendar.react');
+
+ReactDOM.render(<Calendar />, document.getElementById('react-calendar'));
